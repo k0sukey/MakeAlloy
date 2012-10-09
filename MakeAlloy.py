ab4925879a906c402a084beb5f624584583c82ac
Kosuke Isobe <naughtydog0721@gmail.com>
2012-10-09 23:28:15 +0900

    rewrite

--- MakeAlloy.py
+++ MakeAlloy.py
@@ -1,15 +1,36 @@
 import sublime, sublime_plugin, subprocess
 
 class MakeAlloyCommand(sublime_plugin.WindowCommand):
-	s = sublime.load_settings("MakeAlloy.sublime-settings")
-	proc = subprocess.Popen("ls -1 " + s.get("titaniumsdk"), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	out, err = proc.communicate()
-	sdk = out.splitlines()
+	instance_list = ["iphone", "android", "mobileweb", "clean"]
 
 	def run(self, *args, **kwargs):
 		print args, kwargs
-		self.window.show_quick_panel(self.sdk, self._quick_panel_callback)
+		self.window.show_quick_panel(self.instance_list, self._quick_panel_callback)
 
 	def _quick_panel_callback(self, index):
-		root = self.window.folders()[0]
-		self.window.run_command("exec", {"cmd": ["alloy", "-t", self.sdk[index], "run", root]})
+		if (index > -1):
+			root = self.window.folders()[0]
+
+			cat = subprocess.Popen("cat " + root + "/tiapp.xml",
+				stdout=subprocess.PIPE,
+				shell=True)
+
+			grep = subprocess.Popen("grep '<sdk-version>'",
+				stdin=cat.stdout,
+				stdout=subprocess.PIPE,
+				shell=True)
+
+			sed = subprocess.Popen("sed -e 's/<\/*sdk-version>//g'",
+				stdin=grep.stdout,
+				stdout=subprocess.PIPE,
+				shell=True)
+			sdk, err = sed.communicate()
+
+			if (self.instance_list[index] == "clean"):
+				subprocess.call("rm -rf " + root + "/build/iphone/; mkdir -p " + root + "/build/iphone",
+					shell=True)
+				subprocess.call("rm -rf " + root + "/build/android/; mkdir -p " + root + "/build/android",
+					shell=True)
+				sublime.status_message("Deleted build/iphone/*, build/android/*")
+			else:
+				self.window.run_command("exec", {"cmd": ["alloy", "-t", sdk.strip(), "run", root, self.instance_list[index]]})
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Sublime Text 2 plugin.
 cmd + b = build alloy!
+
+
+Inspierd [MakeTi](https://github.com/appersonlabs/MakeTi)
