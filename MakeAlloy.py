4fdbcdd2ba6872928d104c04126db421a1fa3291
Kosuke Isobe <naughtydog0721@gmail.com>
2012-10-12 10:20:53 +0900

    pointing at alloy path

--- MakeAlloy.py
+++ MakeAlloy.py
@@ -1,32 +1,33 @@
 import sublime, sublime_plugin, subprocess
 
 class MakeAlloyCommand(sublime_plugin.WindowCommand):
-	instance_list = ["iphone", "android", "mobileweb", "clean", "generate view", "generate controller", "generate widget", "generate jmk", "generate migration", "generate model"]
+	settings = sublime.load_settings("MakeAlloy.sublime-settings")
+	alloy = settings.get('alloy')
+	panel = ["run iphone", "run android", "run mobileweb", "compile", "clean", "generate view", "generate controller", "generate widget", "generate jmk", "generate migration", "generate model"]
 
 	def run(self, *args, **kwargs):
 		print args, kwargs
-		self.window.show_quick_panel(self.instance_list, self._quick_panel_callback)
+		self.root = self.window.folders()[0]
+		self.window.show_quick_panel(self.panel, self._quick_panel_callback)
 
 	def _quick_panel_callback(self, index):
 		if (index > -1):
-			root = self.window.folders()[0]
-
-			if (self.instance_list[index] == "clean"):
-				subprocess.call("rm -rf " + root + "/build/iphone/; mkdir -p " + root + "/build/iphone",
+			if (self.panel[index] == "clean"):
+				subprocess.call("rm -rf " + self.root + "/build/iphone/; mkdir -p " + self.root + "/build/iphone",
 					shell=True)
-				subprocess.call("rm -rf " + root + "/build/android/; mkdir -p " + root + "/build/android",
+				subprocess.call("rm -rf " + self.root + "/build/android/; mkdir -p " + self.root + "/build/android",
 					shell=True)
 				sublime.status_message("Deleted build/iphone/*, build/android/*")
-			elif (self.instance_list[index] == "generate view"
-				or self.instance_list[index] == "generate controller"
-				or self.instance_list[index] == "generate widget"
-				or self.instance_list[index] == "generate jmk"
-				or self.instance_list[index] == "generate migration"
-				or self.instance_list[index] == "generate model"):
-				self.alloy = "alloy " + self.instance_list[index]
+			elif (self.panel[index] == "generate view"
+				or self.panel[index] == "generate controller"
+				or self.panel[index] == "generate widget"
+				or self.panel[index] == "generate jmk"
+				or self.panel[index] == "generate migration"
+				or self.panel[index] == "generate model"):
+				self.generate = "alloy " + self.panel[index]
 				self.window.show_input_panel(self.alloy, '', self._input_panel_callback, None, None)
 			else:
-				cat = subprocess.Popen("cat " + root + "/tiapp.xml",
+				cat = subprocess.Popen("cat " + self.root + "/tiapp.xml",
 					stdout=subprocess.PIPE,
 					shell=True)
 
@@ -41,9 +42,12 @@ class MakeAlloyCommand(sublime_plugin.WindowCommand):
 					shell=True)
 				sdk, err = sed.communicate()
 
-				self.window.run_command("exec", {"cmd": ["alloy", "-t", sdk.strip(), "run", root, self.instance_list[index]]})
+				if (self.panel[index] == "compile"):
+					self.window.run_command("exec", {"cmd": [self.alloy, "-t", sdk.strip(), "compile", self.root]})
+				else:
+					platform = self.panel[index].split(' ')
+					self.window.run_command("exec", {"cmd": [self.alloy, "-t", sdk.strip(), "run", self.root, platform[1]]})
 
 	def _input_panel_callback(self, text):
-		root = self.window.folders()[0]
-		cmd = self.alloy.split(' ')
-		self.window.run_command("exec", {"cmd": ["alloy", "-o", root + "/app", "generate", cmd[2], text]})
+		cmd = self.generate.split(' ')
+		self.window.run_command("exec", {"cmd": [self.alloy, "-o", self.root + "/app", "generate", cmd[2], text]})
--- README.md
+++ README.md
@@ -4,9 +4,10 @@ MakeAlloy is Sublime Text 2 plugin. cmd + b = build alloy!
 
 1. Make dir ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/MakeAlloy
 2. Copy MakeAlloy.* to ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/MakeAlloy
-3. Restart Sublime Text 2
-4. Go to menu Tools > Build System > select MakeAlloy
-5. cmd + b > select platform
+3. Edit the ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/MakeAlloy/MakeAlloy.sublime-settings file to the correct path for the Alloy
+4. Restart Sublime Text 2
+5. Go to menu Tools > Build System > select MakeAlloy
+6. cmd + b > select platform
 
 ## Inspierd
 
